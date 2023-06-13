829dddfe611a5bd590f8a485c16d0dcfba638981
Knaglobaliasoft <kna.globaliasoft@gmail.com>
2023-06-13 17:29:20 +0530

    add the Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
         path:'MapsUI',
         component:MAPCreatorComponent
     },
+    {
+      path:"Imageeditor",
+      component:ImageeditorComponent
+    }
     
   ];
   @NgModule({
